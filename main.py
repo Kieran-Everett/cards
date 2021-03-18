ab3537215bae98bf588fe1315e185a42960f4a01
Kieran Everett <kieraneverett03@gmail.com>
2021-03-18 23:43:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,34 +2,38 @@
 #import json
 
 
-# Globals
-hp = 1
-deck = []
-
-
 class card():
-    
+    # Card class for saving information about a card
 
     def __init__(self, name, cost, type, value):
 
-        self.name = name
-        self.cost = cost
-        self.type = type
-        self.value = value
+        self.name = name # Internal name of card
+        self.cost = cost # AP cost of card
+        self.type = type # The type of card
+        self.value = value # Variable for storing relevant data
     
     def useCard(self):
+        # Using the card
 
         pass
 
 
 class player():
-
+    # Player class for saving information about the player
     
     def __init__(self, health, ap):
 
-        self.health = health
-        self.ap = ap
-    
+        self.health = health # Player's HP
+        self.ap = ap # Player's AP
+
+        self.deck = [] # Player's deck
+
+
+def useCard(deckID):
+
+    # run deckID.useCard()
+    # destroy deckID
+    pass
 
 def main():
 
